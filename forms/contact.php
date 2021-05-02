1cb2a497ef89b58b9a8595918a2126d094103a04
Akash N O <noakash@ymail.com>
2021-05-02 23:24:35 +0530

    fix php

--- forms/contact.php
+++ forms/contact.php
@@ -3,11 +3,11 @@ if(!empty($_POST["send"])) {
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$subject = $_POST["subject"];
-	$content = $_POST["message"];
+	$message = $_POST["message"];
 
-	$toEmail = "noakash@ymail.com";
+	$to = "noakash@ymail.com";
 	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
-	if(mail($toEmail, $subject, $content)) {
+	if(mail($to, $subject, $message)) {
 	    $message = "Your contact information is received successfully.";
 	    $type = "success";
 	}
