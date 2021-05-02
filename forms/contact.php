d28bb429dcf9b9e569c52ce41252f6b5cbf45d47
Akash N O <noakash@ymail.com>
2021-05-02 23:47:18 +0530

    fix php header

--- forms/contact.php
+++ forms/contact.php
@@ -6,8 +6,12 @@ if(!empty($_POST["send"])) {
 	$message = $_POST["message"];
 
 	$to = "noakash@ymail.com";
-	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
-	if(mail($to, $subject, $message)) {
+  $headers = array("From: from@example.com",
+  "Reply-To: replyto@example.com",
+  "X-Mailer: PHP/" . PHP_VERSION
+);
+$headers = implode("\r\n", $headers);
+	if(mail($to, $subject, $message,$headers)) {
 	    $message = "Your contact information is received successfully.";
 	    $type = "success";
 	}
