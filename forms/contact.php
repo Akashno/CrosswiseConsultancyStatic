2e5a9ee8012a5bee72ae67e3aa3c9711e4fe3d23
Akash N O <noakash@ymail.com>
2021-05-02 23:57:03 +0530

    fix php

--- forms/contact.php
+++ forms/contact.php
@@ -6,12 +6,14 @@ if(!empty($_POST["send"])) {
 	$message = $_POST["message"];
 
 	$to = "noakash@ymail.com";
-  $headers = array("From: from@example.com",
-  "Reply-To: replyto@example.com",
-  "X-Mailer: PHP/" . PHP_VERSION
+ $header = "From: noreply@example.com\r\n";
+$header.= "MIME-Version: 1.0\r\n";
+$header.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$header.= "X-Priority: 1\r\n";
+
 );
-$headers = implode("\r\n", $headers);
-	if(mail($to, $subject, $message,$headers)) {
+
+	if(mail($to, $subject, $message,$header)) {
 	    $message = "Your contact information is received successfully.";
 	    $type = "success";
 	}
