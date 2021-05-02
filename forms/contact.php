52a1787f69e27724fda6ccc7b5c9cabacdd869cc
Akash N O <noakash@ymail.com>
2021-05-02 23:23:00 +0530

    fix php

--- forms/contact.php
+++ forms/contact.php
@@ -1,15 +1,16 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "noakash@ymail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+<?php
+if(!empty($_POST["send"])) {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$subject = $_POST["subject"];
+	$content = $_POST["message"];
 
-    $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
-   
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+	$toEmail = "noakash@ymail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+
 ?>
